4111b760e82adac9972e7918df5bcc833ed02561
orakili <github@orakili.net>
2021-07-23 12:34:16 +0900

    [RW-35] Extend logic to rewrite URLs in data from API to cover image URLs

--- html/modules/custom/reliefweb_api/src/Services/ReliefWebApiClient.php
+++ html/modules/custom/reliefweb_api/src/Services/ReliefWebApiClient.php
@@ -262,8 +262,9 @@ class ReliefWebApiClient {
           }
 
           // Ensure the URL aliases of the resources point to the current site.
-          // @todo remove as it's mostly for development?
-          static::updateApiAliases($data);
+          if (!empty($data['data'])) {
+            static::updateApiUrls($data['data']);
+          }
 
           // Add the resulting data with same index as the query.
           $results[$index] = $data;
@@ -420,20 +421,29 @@ class ReliefWebApiClient {
   }
 
   /**
-   * Update the host of the resource URL aliases.
+   * Update the host of API URLs.
+   *
+   * Note: this mostly for development to convert the URLs from the API used
+   * for dev (ex: stage) to URLs with the current host and scheme.
    *
    * @param array $data
    *   API data.
    */
-  public static function updateApiAliases(array &$data) {
-    $host = \Drupal::request()->getHost();
+  public static function updateApiUrls(array &$data) {
+    $request = \Drupal::request();
+    $host = $request->getHost();
+    $scheme = $request->getScheme();
 
     if ($host !== 'reliefweb.int') {
-      foreach ($data['data'] as &$item) {
-        if (!empty($item['fields']['url_alias'])) {
-          $alias = $item['fields']['url_alias'];
-          $alias = str_replace('reliefweb.int', $host, $alias);
-          $item['fields']['url_alias'] = $alias;
+      $pattern = '#https?://[^/]+/#';
+      $replacement = $scheme . '://' . $host . '/';
+
+      foreach ($data as $key => $item) {
+        if (is_string($item) && strpos($key, 'url') === 0) {
+          $data[$key] = preg_replace($pattern, $replacement, $item);
+        }
+        elseif (is_array($item)) {
+          static::updateApiUrls($data[$key]);
         }
       }
     }
