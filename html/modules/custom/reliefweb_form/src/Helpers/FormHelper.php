c8af839819a1cbfca3a36e2d4ee7a76422cbaf41
orakili <github@orakili.net>
2021-09-22 11:23:40 +0900

    [RW-121] Add form helper

---
+++ html/modules/custom/reliefweb_form/src/Helpers/FormHelper.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\reliefweb_form\Helpers;
+
+/**
+ * Form helper with methods to ease form manipulation.
+ */
+class FormHelper {
+
+  /**
+   * Remove some values from a field's available options in a form.
+   *
+   * @param array $form
+   *   Form to alter.
+   * @param string $field
+   *   Name of the field to alter.
+   * @param array $values
+   *   List of values to remove from the options.
+   */
+  public static function removeOptions(array &$form, $field, array $values) {
+    if (!isset($form[$field]['widget']['#options']) || empty($values)) {
+      return;
+    }
+
+    $widget = &$form[$field]['widget'];
+
+    // Remove the term id from the field options.
+    foreach ($values as $value) {
+      unset($widget['#options'][$value]);
+    }
+
+    // Ensure the term id is also removed from the list of selected values.
+    if (isset($widget['#default_value'])) {
+      if (is_array($widget['#default_value'])) {
+        foreach ($values as $value) {
+          unset($widget['#default_value'][$value]);
+        }
+      }
+      elseif ($widget['#default_value'] == $value) {
+        $widget['#default_value'] = NULL;
+      }
+    }
+  }
+
+}
