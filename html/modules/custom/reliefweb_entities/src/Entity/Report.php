0d2b5295de43f2b577934ab5646211fd3a2a4523
orakili <github@orakili.net>
2021-11-19 23:50:24 +0900

    [RW-25] Add method on Report entity to get the attachments render array

--- html/modules/custom/reliefweb_entities/src/Entity/Report.php
+++ html/modules/custom/reliefweb_entities/src/Entity/Report.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\reliefweb_entities\Entity;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\node\Entity\Node;
 use Drupal\reliefweb_entities\BundleEntityInterface;
@@ -11,6 +13,7 @@ use Drupal\reliefweb_moderation\EntityModeratedInterface;
 use Drupal\reliefweb_moderation\EntityModeratedTrait;
 use Drupal\reliefweb_revisions\EntityRevisionedInterface;
 use Drupal\reliefweb_revisions\EntityRevisionedTrait;
+use Drupal\reliefweb_utility\Helpers\DateHelper;
 use Drupal\reliefweb_utility\Helpers\UrlHelper;
 
 /**
@@ -92,6 +95,86 @@ class Report extends Node implements BundleEntityInterface, EntityModeratedInter
       '#theme' => 'reliefweb_entities_entity_source_disclaimers',
       '#disclaimers' => $disclaimers,
     ];
+  }
+
+  /**
+   * Get the report attachments.
+   *
+   * @return array
+   *   Render array with the list of attachments.
+   */
+  public function getAttachments() {
+    if (!$this->hasField('field_file') || $this->field_file->isEmpty()) {
+      return [];
+    }
+
+    $build = $this->field_file->view();
+    if (empty($build)) {
+      return [];
+    }
+
+    $formats = [];
+    foreach ($this->field_content_format as $item) {
+      $formats[$item->target_id] = $item->target_id;
+    }
+
+    // The report is an interactive content.
+    if (isset($formats[38974])) {
+      $build['#theme'] = 'reliefweb_file_list__interactive';
+
+      $build['#title'] = $this->t('Screenshot(s) of the interactive content as of @date', [
+        '@date' => DateHelper::format($this->getCreatedTime(), 'custom', 'j m Y'),
+      ]);
+
+      $build['#footer'] = Link::fromTextAndUrl(
+        $this->t('View the interactive content page'),
+        Url::fromUri($this->origin_notes->value, [
+          'attributes' => [
+            'target' => '_blank',
+            'rel' => 'noopener',
+          ],
+        ])
+      );
+
+      foreach ($build['#list'] as $index => &$item) {
+        $description = $item['item']->getFileDescription();
+        if (!empty($description)) {
+          $item['label'] = $this->t('Screenshot @index: @description', [
+            '@index' => $index + 1,
+            '@description' => $description,
+          ]);
+        }
+        else {
+          $item['label'] = $this->t('Screenshot @index', [
+            '@index' => $index + 1,
+          ]);
+        }
+        if (isset($item['preview'])) {
+          $item['preview']['#style_name'] = 'large';
+          $item['preview']['#alt'] = $item['label'];
+        }
+      }
+    }
+    // The report is a map or an infographic.
+    elseif (isset($formats[12]) || isset($formats[12570])) {
+      $label = isset($formats[12]) ? $this->t('Download Map') : $this->t('Download Infographic');
+      $build['#attributes']['class'][] = 'rw-file-list--map';
+      foreach ($build['#list'] as &$item) {
+        if (isset($item['preview'])) {
+          $item['preview']['#style_name'] = 'large';
+        }
+        $item['label'] = $label;
+      }
+    }
+    else {
+      $label = $this->t('Download Report');
+      $build['#attributes']['class'][] = 'rw-file-list--report';
+      foreach ($build['#list'] as &$item) {
+        $item['label'] = $label;
+      }
+    }
+
+    return $build;
   }
 
 }
