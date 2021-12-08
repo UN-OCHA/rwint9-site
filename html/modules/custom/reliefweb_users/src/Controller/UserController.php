2b6a999678f96015882493d79e442da87eba8488
Peter Droogmans (attiks) <peter@attiks.com>
2021-12-08 12:03:48 +0100

    [WIP] RW-176: People page

---
+++ html/modules/custom/reliefweb_users/src/Controller/UserController.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace Drupal\reliefweb_users\Controller;
+
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormState;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+
+/**
+ * User posts controller.
+ */
+class UserController extends ControllerBase {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(Connection $database, FormBuilderInterface $form_builder) {
+    $this->database = $database;
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * Get the page content.
+   *
+   * @return array
+   *   Render array.
+   */
+  public function getContent() {
+    // We want the editors to be able to bookmark a moderation page with
+    // a selection of filters so we set the method as GET.
+    $form_state = new FormState();
+    $form_state->setMethod('GET');
+    $form_state->setProgrammed(TRUE);
+    $form_state->setProcessInput(TRUE);
+    $form_state->disableCache();
+
+    // Build the filters form.
+    $form = $this->formBuilder
+      ->buildForm('\Drupal\reliefweb_users\Form\UserPageFilterForm', $form_state);
+
+    // Filters.
+    $build['filters'] = $form;
+
+    $storage = $form_state->getStorage();
+
+    // List of users.
+    $build['list'] = $this->usersAdminList($storage);
+
+    return $build;
+
+  }
+
+  /**
+   * Build the user table for the user admin page.
+   */
+  protected function usersAdminList($storage) {
+    global $pager_total_items;
+
+    $build = [];
+
+    // Get filter values and options.
+    $filters = $storage['filters'] ?? [];
+    $roles = $storage['roles'];
+    $statuses = $storage['statuses'];
+    $rights = $storage['rights'];
+
+    // Table headers.
+    $header = [
+      'uid' => ['data' => $this->t('ID'), 'field' => 'u.uid', 'sort' => 'desc'],
+      'name' => ['data' => $this->t('Name'), 'field' => 'u.name'],
+      'mail' => ['data' => $this->t('Mail'), 'field' => 'u.mail'],
+      'status' => ['data' => $this->t('Status'), 'field' => 'u.status'],
+      'role' => $this->t('Roles'),
+      'sources' => $this->t('Sources (Job, Training)'),
+      'created' => ['data' => $this->t('Member for'), 'field' => 'u.created'],
+      'access' => ['data' => $this->t('Last access'), 'field' => 'u.access'],
+      'edit' => $this->t('Edit'),
+    ];
+
+    $fields = ['uid', 'name', 'mail', 'status', 'created', 'access'];
+
+    // Get the list of users with pagination.
+    $query = $this->database->select('users_field_data', 'u')->range(0, 30);
+    $query->extend('Drupal\Core\Database\Query\PagerSelectExtender');
+
+    // Header sorting.
+    $query->extend('Drupal\Core\Database\Query\TableSortExtender')->orderByHeader($header);
+
+    // Fields to retrieve.
+    $query->fields('u', $fields);
+
+    // Exclude admin and non-authenticated users.
+    $query->condition('u.uid', 1, '>');
+
+    // Filter the query.
+    if (isset($filters['role'])) {
+      $query->innerJoin('user__roles', 'ur', 'ur.entity_id = u.uid');
+      $query->condition('ur.roles_target_id', array_keys($filters['role']));
+    }
+    if (isset($filters['status'])) {
+      $query->condition('u.status', $filters['status'] === 'active' ? 1 : 0);
+    }
+    if (!empty($filters['name'])) {
+      $query->condition('u.name', '%' . $filters['name'] . '%', 'LIKE');
+    }
+    if (!empty($filters['mail'])) {
+      $query->condition('u.mail', '%' . $filters['mail'] . '%', 'LIKE');
+    }
+    if (!empty($filters['posted'])) {
+      $subquery = $this->database->select('node', 'n')
+        ->fields('n', ['uid'])
+        ->condition('n.type', $filters['posted'])
+        ->distinct();
+      $query->innerJoin($subquery, NULL, '%alias.uid = u.uid');
+    }
+    if (isset($filters['job_rights']) || isset($filters['training_rights'])) {
+      $query->innerJoin('taxonomy_term__field_user_posting_rights', 'fpr', '%alias.field_user_posting_rights_id = u.uid');
+      if (isset($filters['job_rights'])) {
+        $query->condition('fpr.field_user_posting_rights_job', array_search($filters['job_rights'], array_keys($rights)));
+      }
+      if (isset($filters['training_rights'])) {
+        $query->condition('fpr.field_user_posting_rights_training', array_search($filters['training_rights'], array_keys($rights)));
+      }
+    }
+    // $query->groupBy('u.uid');
+    // Get the users.
+    $users = $query->execute()->fetchAllAssoc('uid', \PDO::FETCH_OBJ);
+
+    // Prepare the table rows.
+    $rows = [];
+    if (!empty($users)) {
+      $users_roles = $this->database->select('user__roles', 'ur')
+        ->fields('ur', ['entity_id', 'roles_target_id'])
+        ->condition('ur.entity_id', array_keys($users), 'IN')
+        ->execute()
+        ->fetchAll(\PDO::FETCH_COLUMN | \PDO::FETCH_GROUP);
+
+      // Get the sources and posting rights.
+      $this->getUserSources($users);
+
+      /** @var \Drupal\Core\Datetime\DateFormatterInterface $formatter */
+      $date_formatter = \Drupal::service('date.formatter');
+
+      foreach ($users as $user) {
+        $user_roles = [];
+        if (!empty($users_roles[$user->uid])) {
+          $user_roles = array_intersect_key($roles, array_flip($users_roles[$user->uid]));
+        }
+
+        $rows[$user->uid] = [
+          'uid' => $user->uid,
+          'name' => Link::fromTextAndUrl($user->name, URL::fromUserInput('/user/' . $user->uid)),
+          'mail' => Link::fromTextAndUrl($user->mail, URL::fromUserInput('/user/' . $user->uid)),
+          'status' => $statuses[empty($user->status) ? 'blocked' : 'active'],
+          'role' => !empty($user_roles) ? '<ol><li>' . implode('</li><li>', $user_roles) . '</li></ol>' : '',
+          'sources' => $user->sources ?? '',
+          'created' => $date_formatter->formatInterval(time() - $user->created),
+          'access' => !empty($user->access) ? $this->t('@time ago', [
+            '@time' => $date_formatter->formatInterval(time() - $user->access),
+          ]) : $this->t('never'),
+          'edit' => Link::fromTextAndUrl($this->t('edit'), URL::fromUserInput('/user/' . $user->uid . '/edit')),
+        ];
+      }
+    }
+
+    // Get the number of users for the query.
+    $count = !empty($pager_total_items[0]) ? $pager_total_items[0] : 0;
+
+    // List of users.
+    $build['table'] = [
+      '#theme' => 'table',
+      '#header' => $header,
+      '#caption' => $this->t('@count users found.', ['@count' => number_format($count)]),
+      '#rows' => $rows,
+      '#empty' => $this->t('No users found.'),
+      '#attributes' => [
+        'class' => ['users-list'],
+      ],
+    ];
+
+    // Pager for the list of users.
+    $build['pager'] = [
+      '#theme' => 'pager',
+      '#parameters' => \Drupal::request()->query->all(),
+    ];
+
+    return $build;
+  }
+
+  /**
+   * Get the sources and posting rights records for the given users.
+   *
+   * @param array $users
+   *   List of users to be displayed in the users list. This is an associative
+   *   array keyed by the user ids and with user objects as values. The sources
+   *   will be attached to the objects as html lists.
+   */
+  public function getUserSources(array &$users) {
+    // Get the list of sources with posting rights for the given user ids.
+    $query = $this->database->select('taxonomy_term__field_user_posting_rights', 'f');
+    $query->innerJoin('taxonomy_term_field_data', 'td', 'td.tid = f.entity_id');
+    $query->leftJoin('taxonomy_term__field_shortname', 'fs', "fs.entity_id = f.entity_id");
+    $query->addField('f', 'field_user_posting_rights_id', 'uid');
+    $query->addField('f', 'field_user_posting_rights_job', 'job');
+    $query->addField('f', 'field_user_posting_rights_training', 'training');
+    $query->addField('f', 'entity_id', 'tid');
+    $query->addExpression('COALESCE(fs.field_shortname_value, td.name)', 'name');
+    $query->condition('f.field_user_posting_rights_id', array_keys($users), 'IN');
+    $query->condition('f.bundle', 'source');
+
+    $sources = [];
+    foreach ($query->execute() as $record) {
+      $job = $record->job;
+      $training = $record->training;
+      $link = Link::fromTextAndUrl($record->name, URL::fromUserInput('taxonomy/term/' . $record->tid . '/posting-rights'));
+      $row = '<li data-job="' . $job . '" data-training="' . $training . '">' . $link . '</li>';
+      $sources[$record->uid][$record->tid] = $row;
+    }
+
+    // Add the formatted list of sources to the user objects.
+    foreach ($sources as $uid => $rows) {
+      $users[$uid]->sources = '<ol>' . implode('', $rows) . '</ol>';
+    }
+  }
+
+}
---
+++ html/modules/custom/reliefweb_users/src/Form/UserPageFilterForm.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace Drupal\reliefweb_users\Form;
+
+use Drupal\Core\State\State;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\user\Entity\Role;
+
+/**
+ * Manage subscription for user.
+ */
+class UserPageFilterForm extends FormBase {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The state store.
+   *
+   * @var Drupal\Core\State\State
+   */
+  protected $state;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(Connection $database, State $state) {
+    $this->database = $database;
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('state'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'reliefweb_users_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, AccountInterface $user = NULL) {
+    // Get filter values and options.
+    $filters = $this->getFilters($form_state);
+    $roles = $this->getRoles();
+    $statuses = $this->getStatuses();
+    $rights = $this->getRights();
+    $content_types = $this->getContentTypes();
+
+    $form_state->setStorage([
+      'filters' => $filters,
+      'roles' => $roles,
+      'statuses' => $statuses,
+      'rights' => $rights,
+      'content_types' => $content_types,
+    ]);
+
+    $form['filters'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Filters'),
+    ];
+
+    $form['filters']['role'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Role'),
+      '#options' => $roles,
+      '#default_value' => isset($filters['role']) ? $filters['role'] : '',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['status'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Status'),
+      '#options' => ['any' => 'Any'] + $statuses,
+      '#default_value' => isset($filters['status']) ? $filters['status'] : 'any',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['posted'] = [
+      '#title' => $this->t('Posted'),
+      '#type' => 'checkboxes',
+      '#options' => $content_types,
+      '#default_value' => isset($filters['posted']) ? $filters['posted'] : '',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['job_rights'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Job posting rights'),
+      '#options' => ['any' => 'Any'] + $rights,
+      '#default_value' => isset($filters['job_rights']) ? $filters['job_rights'] : 'any',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['training_rights'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Training posting rights'),
+      '#options' => ['any' => 'Any'] + $rights,
+      '#default_value' => isset($filters['training_rights']) ? $filters['training_rights'] : 'any',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Name'),
+      '#maxlength' => 2048,
+      '#default_value' => isset($filters['name']) ? $filters['name'] : '',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['mail'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Mail'),
+      '#maxlength' => 2048,
+      '#default_value' => isset($filters['mail']) ? $filters['mail'] : '',
+      '#optional' => FALSE,
+    ];
+
+    $form['filters']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Filter'),
+      '#attributes' => ['class' => ['form-actions']],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRebuild();
+  }
+
+  /**
+   * Get and cache the filters for the users admin page.
+   */
+  protected function getFilters(FormStateInterface $form_state) {
+    static $filters;
+
+    if (!isset($filters)) {
+      $inputs = $form_state->getUserInput();
+
+      $roles = $this->getRoles();
+      $statuses = $this->getStatuses();
+      $rights = $this->getRights();
+      $content_types = $this->getContentTypes();
+
+      $filters = [
+        'role' => !empty($inputs['role']) ? array_intersect_key($roles, $inputs['role']) : NULL,
+        'status' => isset($inputs['status'], $statuses[$inputs['status']]) ? $inputs['status'] : NULL,
+        'name' => !empty($inputs['name']) && is_string($inputs['name']) ? $inputs['name'] : NULL,
+        'mail' => !empty($inputs['mail']) && is_string($inputs['mail']) ? $inputs['mail'] : NULL,
+        'posted' => isset($inputs['posted']) && is_array($inputs['posted']) ? array_intersect_key($content_types, $inputs['posted']) : NULL,
+        'job_rights' => isset($inputs['job_rights'], $rights[$inputs['job_rights']]) ? $inputs['job_rights'] : NULL,
+        'training_rights' => isset($inputs['training_rights'], $rights[$inputs['training_rights']]) ? $inputs['training_rights'] : NULL,
+      ];
+    }
+
+    return $filters;
+  }
+
+  /**
+   * Get the list of user roles keyed by id.
+   */
+  protected function getRoles() {
+    static $roles;
+
+    if (!isset($roles)) {
+      $role_objects = Role::loadMultiple();
+      $roles = array_combine(array_keys($role_objects), array_map(function ($a) {
+        return $a->label();
+
+      }, $role_objects));
+    }
+
+    return $roles;
+  }
+
+  /**
+   * Get the list of content types.
+   */
+  protected function getContentTypes() {
+    return [
+      'job' => $this->t('Job'),
+      'training' => $this->t('Training'),
+      'report' => $this->t('Report'),
+    ];
+  }
+
+  /**
+   * Get the list of possible user statuses.
+   */
+  protected function getStatuses() {
+    return [
+      'blocked' => $this->t('Blocked'),
+      'active' => $this->t('Active'),
+    ];
+  }
+
+  /**
+   * Get the list of possible user statuses.
+   */
+  protected function getRights() {
+    return [
+      'unverified' => $this->t('Unverified'),
+      'blocked' => $this->t('Blocked'),
+      'allowed' => $this->t('Allowed'),
+      'trusted' => $this->t('Trusted'),
+    ];
+  }
+
+}
