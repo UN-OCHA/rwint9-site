402440bbd544e1e2406336c3445b3577ceae18f4
orakili <github@orakili.net>
2022-01-21 03:47:07 +0000

    [RW-291] Add image migration

--- html/modules/custom/reliefweb_migrate/src/Commands/ReliefWebMigrateCommands.php
+++ html/modules/custom/reliefweb_migrate/src/Commands/ReliefWebMigrateCommands.php
@@ -6,11 +6,14 @@ use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Consolidation\SiteAlias\SiteAliasManagerAwareInterface;
 use Consolidation\SiteAlias\SiteAliasManagerAwareTrait;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigInstallerInterface;
 use Drupal\Core\Config\ConfigManager;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Datetime\DateFormatter;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 use Drupal\migrate\Exception\RequirementsException;
 use Drupal\migrate\Plugin\MigrationPluginManager;
@@ -18,6 +21,7 @@ use Drupal\migrate\Plugin\RequirementsInterface;
 use Drupal\reliefweb_migrate\Plugin\migrate\source\SourceMigrationStatusInterface;
 use Drupal\reliefweb_utility\Helpers\LegacyHelper;
 use Drush\Commands\DrushCommands;
+use GuzzleHttp\Psr7\Utils;
 
 /**
  * ReliefWeb migration Drush commandfile.
@@ -41,6 +45,13 @@ class ReliefWebMigrateCommands extends DrushCommands implements SiteAliasManager
   protected $configManager;
 
   /**
+   * The database.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
    * Date formatter service.
    *
    * @var \Drupal\Core\Datetime\DateFormatter
@@ -53,6 +64,13 @@ class ReliefWebMigrateCommands extends DrushCommands implements SiteAliasManager
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
+
+  /**
+   * The file system.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
 
   /**
    * Key-value store service.
@@ -74,16 +92,20 @@ class ReliefWebMigrateCommands extends DrushCommands implements SiteAliasManager
   public function __construct(
     ConfigInstallerInterface $config_installer,
     ConfigManager $config_manager,
+    Connection $database,
     DateFormatter $date_formatter,
     EntityTypeManagerInterface $entity_type_manager,
+    FileSystemInterface $file_system,
     KeyValueFactoryInterface $key_value,
     MigrationPluginManager $migration_plugin_manager
   ) {
     parent::__construct();
     $this->configInstaller = $config_installer;
     $this->configManager = $config_manager;
+    $this->database = $database;
     $this->dateFormatter = $date_formatter;
     $this->entityTypeManager = $entity_type_manager;
+    $this->fileSystem = $file_system;
     $this->keyValue = $key_value;
     $this->migrationPluginManager = $migration_plugin_manager;
   }
@@ -330,6 +352,170 @@ class ReliefWebMigrateCommands extends DrushCommands implements SiteAliasManager
       }
     }
     return $migrations ?? [];
+  }
+
+  /**
+   * Migrate the images.
+   *
+   * @command rw-migrate:migrate-images
+   *
+   * @option base_url The base url of the site from which to retrieve the files.
+   * @option batch_size Number of reports with images to process at once.
+   * @option limit Maximum number of reports with non migrated files to process,
+   *   0 means process everything.
+   * @option delete Delete the old -> new uri mapping.
+   *
+   * @default options [
+   *   'base_url' => 'https://reliefweb.int',
+   *   'batch_size' => 1000,
+   *   'limit' => 0,
+   *   'delete' => FALSE,
+   * ]
+   *
+   * @aliases rw-mmi,rw-migrate-migrate-images
+   *
+   * @usage rw-migrate:migrate-images
+   *   Download the images.
+   *
+   * @validate-module-enabled reliefweb_migrate
+   */
+  public function migrateImages($options = [
+    'base_url' => 'https://reliefweb.int',
+    'batch_size' => 1000,
+    'limit' => 0,
+    'delete' => FALSE,
+  ]) {
+    $base_url = $options['base_url'];
+    $batch_size = (int) $options['batch_size'];
+    $limit = (int) $options['limit'];
+
+    if (preg_match('#^https?://[^/]+$#', $base_url) !== 1) {
+      $this->logger()->error(dt('The base url must be in the form http(s)://example.test.'));
+      return FALSE;
+    }
+    if ($batch_size < 1 || $batch_size > 1000) {
+      $this->logger()->error(dt('The batch size must be within 1 and 1000.'));
+      return FALSE;
+    }
+    if ($limit < 0) {
+      $this->logger()->error(dt('The limit must be equal or superior to 0.'));
+      return FALSE;
+    }
+
+    // Get the attachment field table.
+    $table = 'reliefweb_migrate_uri_mapping';
+
+    // Retrieve the most recent report node with attachments.
+    $query = $this->database->select($table, $table);
+    $query->addExpression('MAX(' . $table . '.id)');
+    $last = $query->execute()?->fetchField();
+
+    if (empty($last)) {
+      $this->logger()->info(dt('No images to download found.'));
+      return TRUE;
+    }
+
+    $last = $last + 1;
+    $count_images = 0;
+    $count_downloaded = 0;
+
+    while ($last !== NULL) {
+      $records = $this->database
+        ->select($table, $table)
+        ->fields($table, ['id', 'new_uri', 'old_uri'])
+        ->orderBy('id', 'DESC')
+        ->condition('id', $last, '<')
+        ->range(0, $limit > 0 ? min($limit - $count_images, $batch_size) : $batch_size)
+        ->execute()
+        ?->fetchAllAssoc('id', \PDO::FETCH_ASSOC) ?? [];
+
+      if (!empty($records)) {
+        $last = array_key_last($records);
+        $count_images += count($records);
+
+        foreach ($records as $record) {
+          $source = str_replace('public://', '', $record['old_uri']);
+          $source = UrlHelper::encodePath($source);
+          $source = $base_url . '/sites/default/files/' . $source;
+          $result = $this->downloadFile($source, $record['new_uri']);
+          $count_downloaded += $result ? 1 : 0;
+        }
+
+        // Remove the entries form the url mapping.
+        // @todo check if there is a different way to mark files as downloaded.
+        if (!empty($options['delete'])) {
+          $this->database->delete($table)
+            ->condition('id', array_keys($records), 'IN')
+            ->execute();
+        }
+      }
+      else {
+        $last = NULL;
+        break;
+      }
+
+      if ($limit > 0 && $count_images >= $limit) {
+        break;
+      }
+    }
+
+    $this->logger()->info(dt('Successfully downloaded @count_downloaded out of @count_images images.', [
+      '@count_downloaded' => $count_downloaded,
+      '@count_images' => $count_images,
+    ]));
+    return TRUE;
+  }
+
+  /**
+   * Download a file to its local location.
+   *
+   * @param string $source
+   *   Source file URI.
+   * @param string $destination
+   *   Destination file URI.
+   *
+   * @return bool
+   *   TRUE if the file could be downloaded.
+   */
+  protected function downloadFile($source, $destination) {
+    // Try to download the file.
+    $success = FALSE;
+    $directory = dirname($destination);
+    if ($this->fileSystem->prepareDirectory($directory, $this->fileSystem::CREATE_DIRECTORY)) {
+      try {
+        $input = Utils::TryFopen($source, 'r');
+        $output = Utils::TryFopen($destination, 'w');
+        $success = stream_copy_to_stream($input, $output);
+      }
+      catch (\Exception $exception) {
+        $this->logger()->error(dt('Unable to download file @source to @destination: @message.', [
+          '@source' => $source,
+          '@destination' => $destination,
+          '@message' => $exception->getMessage(),
+        ]));
+        return FALSE;
+      }
+    }
+    else {
+      $this->logger()->error(dt('Unable to create directory for @destination.', [
+        '@destination' => $destination,
+      ]));
+      return FALSE;
+    }
+
+    if (empty($success)) {
+      $this->logger()->error(dt('Unable to download file @source to @destination.', [
+        '@source' => $source,
+        '@destination' => $destination,
+      ]));
+    }
+    else {
+      $this->logger()->info(dt('Successfully downloaded file @source to @destination.', [
+        '@source' => $source,
+        '@destination' => $destination,
+      ]));
+    }
+    return $success;
   }
 
   /**
