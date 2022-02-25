e727b196d0bb6c80ea80a74d8239b8dbcc1db6cf
orakili <github@orakili.net>
2022-02-25 07:27:03 +0000

    [RW-155] Fix issues with markdown conversion to work like previous libraries used on D7

--- html/modules/custom/reliefweb_utility/src/Plugin/Filter/Markdown.php
+++ html/modules/custom/reliefweb_utility/src/Plugin/Filter/Markdown.php
@@ -7,7 +7,7 @@ use Drupal\Core\Url;
 use Drupal\Core\Render\Markup;
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
-use League\CommonMark\Environment;
+use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\Attributes\AttributesExtension;
 use League\CommonMark\Extension\Autolink\AutolinkExtension;
 use League\CommonMark\Extension\ExternalLink\ExternalLinkExtension;
@@ -27,13 +27,132 @@ use League\CommonMark\MarkdownConverter;
 class Markdown extends FilterBase {
 
   /**
+   * List of HTML block level elements.
+   *
+   * @var array
+   */
+  public static $htmlBlockElements = [
+    "address",
+    "article",
+    "aside",
+    "base",
+    "basefont",
+    "blockquote",
+    "body",
+    "caption",
+    "center",
+    "col",
+    "colgroup",
+    "dd",
+    "details",
+    "dialog",
+    "dir",
+    "div",
+    "dl",
+    "dt",
+    "fieldset",
+    "figcaption",
+    "figure",
+    "footer",
+    "form",
+    "frame",
+    "frameset",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "head",
+    "header",
+    "hr",
+    "html",
+    "iframe",
+    "legend",
+    "li",
+    "link",
+    "main",
+    "menu",
+    "menuitem",
+    "nav",
+    "noframes",
+    "ol",
+    "optgroup",
+    "option",
+    "p",
+    "param",
+    "section",
+    "source",
+    "summary",
+    "table",
+    "tbody",
+    "td",
+    "tfoot",
+    "th",
+    "thead",
+    "title",
+    "tr",
+    "track",
+    "ul",
+    // Extra elements that need to be followed by a blank line for the following
+    // text to be converted to markdown.
+    "pre",
+    "script",
+    "style",
+    "textarea",
+  ];
+
+  /**
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
+    $html = static::convertToHtml($text, [
+      \Drupal::request()->getHost(),
+      'reliefweb.int',
+    ]);
+    return new FilterProcessResult($html);
+  }
+
+  /**
+   * Convert a markdown text to HTML.
+   *
+   * @param string $text
+   *   Markdown text to convert.
+   * @param array $internal_hosts
+   *   List of internal hosts to determine if a link is external or not.
+   *
+   * @return string
+   *   HTML.
+   *
+   * @todo this is the same code as \RWApiIndexer\Processor::convertToHtml().
+   *   See if we can consolidate that somehow while keeping this and the API
+   *   indexer independent. In the meantime any change here should probably
+   *   reflected in the API indexer.
+   *
+   * @see \RWApiIndexer\Processor::convertToHtml()
+   */
+  public static function convertToHtml($text, array $internal_hosts = ['reliefweb.int']) {
+    static $pattern;
+
+    // CommonMark specs consider text following an HTML block element without
+    // a blank line to seperate them, as part of the HTML block. This is a
+    // breaking change compared to what Michel Fortin's PHP markdown or
+    // hoedown libraries were doing so use the following regex to ensure there
+    // is a blank line.
+    // @see https://spec.commonmark.org/0.30/#html-blocks
+    // @see https://talk.commonmark.org/t/beyond-markdown/2787/4
+    if (!isset($pattern)) {
+      $pattern = '#(</' . implode('>|</', static::$htmlBlockElements) . '>)\s*#m';
+    }
+    $text = preg_replace($pattern, "$1\n\n", $text);
+
     // Add a space before the heading '#' which is fine as ReliefWeb doesn't use
     // hash tags.
     // @see https://talk.commonmark.org/t/heading-not-working/819/42
     $text = preg_replace('/^(#+)([^# ])/m', '$1 $2', $text);
+
+    // No need for extra blanks.
+    $text = trim($text);
 
     // Obtain a pre-configured Environment with all the CommonMark
     // parsers/renderers ready-to-go.
@@ -42,10 +161,7 @@ class Markdown extends FilterBase {
     // Configuration to add attributes to external links.
     $external_link_config = [
       'external_link' => [
-        'internal_hosts' => [
-          \Drupal::request()->getHost(),
-          'reliefweb.int',
-        ],
+        'internal_hosts' => $internal_hosts,
         'open_in_new_window' => TRUE,
       ],
     ];
@@ -67,9 +183,7 @@ class Markdown extends FilterBase {
     $converter = new MarkdownConverter($environment);
 
     // Convert to HTML.
-    $html = $converter->convertToHtml($text);
-
-    return new FilterProcessResult($html);
+    return $converter->convertToHtml($text);
   }
 
   /**
