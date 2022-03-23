24d7623409af6e02ed40ef5276d1c352d858d548
orakili <github@orakili.net>
2022-03-23 06:33:36 +0000

    [RW-164] Update reliefweb_fields module to use the user email fields

--- html/modules/custom/reliefweb_fields/src/Plugin/Field/FieldWidget/ReliefWebUserPostingRights.php
+++ html/modules/custom/reliefweb_fields/src/Plugin/Field/FieldWidget/ReliefWebUserPostingRights.php
@@ -101,10 +101,16 @@ class ReliefWebUserPostingRights extends WidgetBase implements ContainerFactoryP
 
       // Retrieve the user data.
       if (!empty($ids)) {
-        $users = \Drupal::database()
+        $query = \Drupal::database()
           ->select('users_field_data', 'u')
           ->fields('u', ['uid', 'name', 'mail', 'status'])
-          ->condition('u.uid', $ids, 'IN')
+          ->condition('u.uid', $ids, 'IN');
+        $query->leftJoin('user__field_email', 'fe', 'fe.entity_id = u.uid');
+        $query->fields('fe', ['field_email_value']);
+        $query->leftJoin('user__field_email_confirmed', 'fec', 'fec.entity_id = u.uid');
+        $query->fields('fec', ['field_email_confirmed_value']);
+
+        $users = $query
           ?->execute()
           ?->fetchAllAssoc('uid', \PDO::FETCH_ASSOC);
 
@@ -171,7 +177,14 @@ class ReliefWebUserPostingRights extends WidgetBase implements ContainerFactoryP
     $data['notes'] = isset($data['notes']) ? trim($data['notes']) : '';
 
     $data['name'] = trim($data['name']);
-    $data['mail'] = trim($data['mail']);
+
+    if (!empty($data['field_email_confirmed_value']) && !empty($data['field_email_value'])) {
+      $data['mail'] = trim($data['field_email_value']);
+    }
+    else {
+      $data['mail'] = trim($data['mail']);
+    }
+
     $data['status'] = intval($data['status'], 10);
     // Blocked users are not allowed to post.
     if ($data['status'] === 0) {
@@ -307,7 +320,7 @@ class ReliefWebUserPostingRights extends WidgetBase implements ContainerFactoryP
 
     $uid = $user->id();
     $blocked = $user->isBlocked();
-    $email_changed = isset($user->original) && $user->getEmail() !== $user->original->getEmail();
+    $email_changed = isset($user->original) && reliefweb_users_get_email($user) !== reliefweb_users_get_email($user->original);
     $message = '';
     $date = date_format(date_create(), 'Y-m-d');
 
