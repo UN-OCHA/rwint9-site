8af370596060d5d2c8ca6e518a52e0a58f13417f
orakili <github@orakili.net>
2022-06-15 06:32:47 +0000

    fix: ensure revisions with just a log message change are displayed in the revision table
    Refs: RW-580

--- html/modules/custom/reliefweb_revisions/src/EntityRevisionedTrait.php
+++ html/modules/custom/reliefweb_revisions/src/EntityRevisionedTrait.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\reliefweb_revisions;
 
+use Drupal\Core\Entity\ContentEntityInterface;
+
 /**
  * Provides a trait for the entity revision history.
  */
@@ -43,6 +45,47 @@ trait EntityRevisionedTrait {
       $this->entityHistory = \Drupal::service('reliefweb_revisions.entity.history');
     }
     return $this->entityHistory;
+  }
+
+  /**
+   * Returns an array of field names to skip when checking for changes.
+   *
+   * Note: the method's name is prefixed with `trait` so it can override the
+   * original trait method. See ContentEntityBase.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   A content entity object.
+   *
+   * @return string[]
+   *   An array of field names.
+   *
+   * @see \Drupal\Core\Entity\EntityChangesDetectionTrait::getFieldsToSkipFromTranslationChangesCheck()
+   * @see \Drupal\Core\Entity\ContentEntityBase::getFieldsToSkipFromTranslationChangesCheck()
+   */
+  protected function traitGetFieldsToSkipFromTranslationChangesCheck(ContentEntityInterface $entity) {
+    /** @var \Drupal\Core\Entity\ContentEntityTypeInterface $entity_type */
+    $entity_type = $entity->getEntityType();
+
+    $revision_keys = $entity_type->getRevisionMetadataKeys();
+    // A new revision comment is considered a change.
+    unset($revision_keys['revision_log_message']);
+
+    // A list of known revision metadata fields which should be skipped from
+    // the comparison.
+    $fields = [
+      $entity_type->getKey('revision'),
+      $entity_type->getKey('revision_translation_affected'),
+    ];
+    $fields = array_merge($fields, array_values($revision_keys));
+
+    // Computed fields should be skipped by the check for translation changes.
+    foreach (array_diff_key($entity->getFieldDefinitions(), array_flip($fields)) as $field_name => $field_definition) {
+      if ($field_definition->isComputed()) {
+        $fields[] = $field_name;
+      }
+    }
+
+    return $fields;
   }
 
 }
