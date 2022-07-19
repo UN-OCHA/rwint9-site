f79028074ed72d868cffecd7ec9e32579ebabcf9
orakili <github@orakili.net>
2022-07-19 13:32:13 +0000

    feat: add helper methods to get the bundle label of an entity
    Refs: RW-513

--- html/modules/custom/reliefweb_utility/src/Helpers/EntityHelper.php
+++ html/modules/custom/reliefweb_utility/src/Helpers/EntityHelper.php
@@ -46,6 +46,35 @@ class EntityHelper {
   }
 
   /**
+   * Get a bundle's label from an entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   Entity.
+   *
+   * @return string
+   *   Bundle label.
+   */
+  public static function getBundleLabelFromEntity(EntityInterface $entity) {
+    return static::getBundleLabel($entity->getEntityTypeId(), $entity->bundle());
+  }
+
+  /**
+   * Get a bundle's label.
+   *
+   * @param string $entity_type_id
+   *   Entity type ID.
+   * @param string $bundle
+   *   Entity bundle.
+   *
+   * @return string
+   *   Bundle label.
+   */
+  public static function getBundleLabel($entity_type_id, $bundle) {
+    $bundle_info = \Drupal::service('entity_type.bundle.info')->getBundleInfo($entity_type_id);
+    return $bundle_info[$bundle]['label'] ?? $bundle;
+  }
+
+  /**
    * Format a revision log message.
    *
    * @param string $message
