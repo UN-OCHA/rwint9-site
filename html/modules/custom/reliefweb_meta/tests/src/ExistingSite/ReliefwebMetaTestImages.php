7691840e7df8997617210cb96e0ad5c58e7a3b6d
Peter Droogmans (attiks) <peter@attiks.com>
2024-01-10 16:17:22 +0100

    test: Test meta images
    Refs: #RW-281

--- html/modules/custom/reliefweb_meta/tests/src/ExistingSite/ReliefwebMetaTestImages.php
+++ html/modules/custom/reliefweb_meta/tests/src/ExistingSite/ReliefwebMetaTestImages.php
@@ -5,13 +5,11 @@
 namespace Drupal\Tests\reliefweb_meta\ExistingSite;
 
 use Drupal\Core\File\FileSystemInterface;
-use Drupal\Core\Url;
-use Drupal\file\Entity\File;
 use Drupal\media\Entity\Media;
 use Drupal\node\Entity\Node;
 use Drupal\reliefweb_moderation\EntityModeratedInterface;
+use Drupal\taxonomy\Entity\Term;
 use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
-use Drupal\theme_switcher\Entity\ThemeSwitcherRule;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
@@ -28,13 +26,13 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
     global $base_url;
 
     $site_name = \Drupal::config('system.site')->get('name');
-    $report_title = 'My report';
+    $report_title = 'Test default image';
 
     $report = Node::create([
       'type' => 'report',
       'title' => $report_title,
     ]);
-    $report->isNew();
+
     $report->save();
 
     if ($report instanceof EntityModeratedInterface) {
@@ -57,7 +55,7 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
     global $base_url;
 
     $site_name = \Drupal::config('system.site')->get('name');
-    $report_title = 'My report';
+    $report_title = 'Test headline image';
     $media = $this->createMediaImage('headline.jpg');
     $filename = $media->field_media_image->entity->getFileUri();
 
@@ -68,7 +66,7 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
         'target_id' => $media->id(),
       ],
     ]);
-    $report->isNew();
+
     $report->save();
 
     if ($report instanceof EntityModeratedInterface) {
@@ -91,7 +89,7 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
     global $base_url;
 
     $site_name = \Drupal::config('system.site')->get('name');
-    $report_title = 'My report';
+    $report_title = 'Test normal image';
     $media = $this->createMediaImage('image.jpg');
     $filename = $media->field_media_image->entity->getFileUri();
 
@@ -102,7 +100,7 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
         'target_id' => $media->id(),
       ],
     ]);
-    $report->isNew();
+
     $report->save();
 
     if ($report instanceof EntityModeratedInterface) {
@@ -116,6 +114,110 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
     $this->drupalGet($report->toUrl());
     $this->assertSession()->titleEquals($report_title . ' | ' . $site_name);
     $this->assertSession()->responseContains('<meta property="og:image" content="' . $base_url . '/sites/default/files/styles/large/public/' . str_replace('public://', '', $filename));
+  }
+
+  /**
+   * Test report - disaster type.
+   */
+  public function testDisaserTypeImage() {
+    global $base_url;
+
+    $site_name = \Drupal::config('system.site')->get('name');
+    $report_title = 'Test disaster type image';
+
+    $disaster_type = $this->createDisasterTypeTerm();
+
+    $report = Node::create([
+      'type' => 'report',
+      'title' => $report_title,
+      'field_disaster_type' => [
+        'target_id' => $disaster_type->id(),
+      ],
+    ]);
+
+    $report->save();
+
+    if ($report instanceof EntityModeratedInterface) {
+      $report->setModerationStatus('published');
+    }
+
+    $report->setPublished();
+    $report->save();
+
+    // Check operation on landing page.
+    $this->drupalGet($report->toUrl());
+    $this->assertSession()->titleEquals($report_title . ' | ' . $site_name);
+    $this->assertSession()->responseContains('<meta property="og:image" content="' . $base_url . '/modules/custom/reliefweb_meta/images/disaster-type/CW.png');
+  }
+
+  /**
+   * Test report - primary country.
+   */
+  public function testPrimaryCountryImage() {
+    global $base_url;
+
+    $site_name = \Drupal::config('system.site')->get('name');
+    $report_title = 'Test primary country image';
+    $iso3 = 'bel';
+
+    $country = $this->createCountryTerm($iso3);
+
+    $report = Node::create([
+      'type' => 'report',
+      'title' => $report_title,
+      'field_primary_country' => [
+        'target_id' => $country->id(),
+      ],
+    ]);
+
+    $report->save();
+
+    if ($report instanceof EntityModeratedInterface) {
+      $report->setModerationStatus('published');
+    }
+
+    $report->setPublished();
+    $report->save();
+
+    // Check operation on landing page.
+    $this->drupalGet($report->toUrl());
+    $this->assertSession()->titleEquals($report_title . ' - ' . $iso3 . ' | ' . $site_name);
+    $this->assertSession()->responseContains('<meta property="og:image" content="' . $base_url . '/modules/custom/reliefweb_meta/images/icons/Belgium_BEL.png');
+  }
+
+  /**
+   * Test report - country.
+   */
+  public function testCountryImage() {
+    global $base_url;
+
+    $site_name = \Drupal::config('system.site')->get('name');
+    $report_title = 'Test country image';
+    $iso3 = 'afg';
+
+    $country = $this->createCountryTerm($iso3);
+
+    $report = Node::create([
+      'type' => 'report',
+      'title' => $report_title,
+      'field_country' => [
+        'target_id' => $country->id(),
+      ],
+    ]);
+
+    $report->save();
+
+    if ($report instanceof EntityModeratedInterface) {
+      $report->setModerationStatus('published');
+    }
+
+    $report->setPublished();
+    $report->save();
+
+    // Check operation on landing page.
+    $this->drupalGet($report->toUrl());
+    $this->assertSession()->titleEquals($report_title . ' | ' . $site_name);
+    $this->assertSession()->responseContains('<meta property="og:image" content="' . $base_url . '/modules/custom/reliefweb_meta/images/icons/Afghanistan_AFG.png');
   }
 
   /**
@@ -144,6 +246,43 @@ class ReliefwebMetaTestImages extends ExistingSiteBase {
     $media->save();
 
     return $media;
+  }
+
+  /**
+   * Create disaster type term.
+   */
+  private function createDisasterTypeTerm() : Term {
+    $term = Term::create([
+      'vid' => 'disaster_type',
+      'name' => 'Coldwave',
+      'field_disaster_type_code' => 'CW'
+    ]);
+
+    $term->save();
+    $term->setPublished()->save();
+
+    return $term;
+  }
+
+  /**
+   * Create country term.
+   */
+  private function createCountryTerm($iso3) : Term {
+    $term = Term::create([
+      'vid' => 'country',
+      'name' => $iso3,
+      'field_iso3' => $iso3,
+    ]);
+
+    $term->save();
+
+    if ($term instanceof EntityModeratedInterface) {
+      $term->setModerationStatus('ongoing');
+    }
+
+    $term->setPublished()->save();
+
+    return $term;
   }
 
 }
