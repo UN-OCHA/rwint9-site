35cb9c945bef2c3f069d49710b374b4a5e236f78
Peter Droogmans (attiks) <peter@attiks.com>
2025-08-12 15:05:47 +0200

    feat: Import organizations
    Refs: #RW-1332

---
+++ html/modules/custom/reliefweb_sync_orgs/src/Form/ImportAndQueueCsvFile.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Drupal\reliefweb_sync_orgs\Form;
+
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelTrait;
+use Drupal\Core\Queue\QueueInterface;
+use Drupal\file\Validation\FileValidatorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a form to import a csv file and add all items to a queue.
+ */
+class ImportAndQueueCsvFile extends FormBase {
+
+  use LoggerChannelTrait;
+
+  /**
+   * Queue name.
+   */
+  protected const QUEUE_NAME = 'reliefweb_sync_orgs_process_csv_item';
+
+  /**
+   * Queue factory.
+   *
+   * @var \Drupal\Core\Queue\QueueInterface
+   */
+  protected $queue;
+
+  /**
+   * File system.
+   *
+   * @var Drupal\Core\File\FileSystem
+   */
+  protected $fileSystem;
+
+  /**
+   * File validator service.
+   *
+   * @var \Drupal\file\Validation\FileValidatorInterface
+   */
+  protected $fileValidator;
+
+  /**
+   * Constructs a new InoreaderImportForm.
+   */
+  public function __construct(
+    QueueInterface $queue_factory,
+    FileSystemInterface $file_system,
+    FileValidatorInterface $file_validator,
+  ) {
+    $this->queue = $queue_factory;
+    $this->fileSystem = $file_system;
+    $this->fileValidator = $file_validator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('queue')->get(self::QUEUE_NAME),
+      $container->get('file_system'),
+      $container->get('file.validator'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'reliefweb_sync_orgs_import_and_queue_csv_file';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Allow user to select the source of the CSV file.
+    $form['source'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Source'),
+      '#options' => [
+        'hdx' => $this->t('HDX'),
+        'hpc' => $this->t('HPC tools'),
+      ],
+      '#required' => TRUE,
+    ];
+
+    $form['csv_file'] = [
+      '#type' => 'file',
+      '#title' => $this->t('CSV file'),
+      '#description' => $this->t('CSV file containing clients to import'),
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Import'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $all_files = $this->getRequest()->files->get('files', []);
+    if (!empty($all_files['csv_file'])) {
+      $file_upload = $all_files['csv_file'];
+      if ($file_upload->isValid()) {
+        $form_state->setValue('csv_file', $file_upload->getRealPath());
+        return;
+      }
+    }
+
+    $form_state->setErrorByName('csv_file', $this->t('The file could not be uploaded.'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $validators = ['FileExtension' => ['csv']];
+    $file = file_save_upload('csv_file', $validators, FALSE, 0);
+    if (!$file) {
+      return;
+    }
+
+    $filename = $this->fileSystem->realpath($file->getFileUri());
+    $source = $form_state->getValue('source');
+
+    $this->importFromCsv($filename, $source);
+  }
+
+  /**
+   * Import from csv.
+   */
+  public function importFromCsv(string $filename, string $source) {
+    $count = 0;
+
+    $f = fopen($filename, 'r');
+    $header = fgetcsv($f, NULL, ',');
+    $header_lowercase = array_map('strtolower', $header);
+    $header_lowercase = array_map('trim', $header_lowercase);
+
+    // Get data.
+    while ($row = fgetcsv($f, NULL, ',')) {
+      $data = [];
+      for ($i = 0; $i < count($row); $i++) {
+        $data[$header_lowercase[$i]] = trim($row[$i] ?? '');
+      }
+
+      // Add source to the data.
+      $data['_source'] = $source;
+
+      // Add row number to the data.
+      $data['_row_number'] = $count + 1;
+
+      $this->queue->createItem($data);
+      $count++;
+    }
+
+    fclose($f);
+
+    $message = $this->t('Queued @count items.', [
+      '@count' => $count,
+    ]);
+
+    $this->messenger()->addMessage($message);
+  }
+
+}
---
+++ html/modules/custom/reliefweb_sync_orgs/src/Plugin/QueueWorker/ProcessCsvItem.php
@@ -0,0 +1,402 @@
+<?php
+
+namespace Drupal\reliefweb_sync_orgs\Plugin\QueueWorker;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Processes a single item from the CSV import queue.
+ *
+ * @QueueWorker(
+ *   id = "reliefweb_sync_orgs_process_csv_item",
+ *   title = @Translation("Process ReliefWeb Sync Orgs CSV Item"),
+ *   cron = {"time" = 60}
+ * )
+ */
+class ProcessCsvItem extends QueueWorkerBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Main constructor.
+   *
+   * @param array $configuration
+   *   Configuration array.
+   * @param mixed $plugin_id
+   *   The plugin id.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityTypeManager = $entity_type_manager;
+    $this->database = $database;
+  }
+
+  /**
+   * Used to grab functionality from the container.
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('database'),
+    );
+  }
+
+  /**
+   * Processes an item in the queue.
+   *
+   * @param mixed $queue_item
+   *   The queue item data.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Exception
+   */
+  public function processItem($queue_item) {
+    $this->mergeOrganizationTerm($queue_item);
+  }
+
+  /**
+   * Try to merge with existing term, or create a new one.
+   */
+  protected function mergeOrganizationTerm($item) {
+    $source = $item['_source'] ?? '';
+
+    switch ($source) {
+      case 'hdx':
+        $this->mergeHdxOrganizationTerm($item);
+        break;
+
+      case 'hpc':
+        $this->mergeHpcOrganizationTerm($item);
+        break;
+
+      default:
+        throw new \Exception("Unsupported source: $source");
+    }
+  }
+
+  /**
+   * Merges an organization term based on a HDX item.
+   */
+  protected function mergeHdxOrganizationTerm($item) {
+    $source = 'hdx';
+    $id = $item['id'];
+    $term = NULL;
+
+    $import_record = $this->getExistingImportRecord($source, $id);
+    if (empty($import_record)) {
+      $import_record = $this->constructReliefwebSyncOrgsRecord($source, $id, $item);
+    }
+
+    // If fts_id is available, try to load the term by fts_id.
+    if (!empty($item['fts_id'])) {
+      $term = $this->loadSourceTermByFtsId($item['fts_id']);
+    }
+
+    // If term is not found by fts_id, try to load it by id.
+    if (empty($term)) {
+      // List of possible fields to check for existing terms.
+      $fields_to_check = [
+        'display_name',
+        'name',
+        'title',
+      ];
+
+      // Remove " (inactive)" from display_name and name fields.
+      foreach ($fields_to_check as $field) {
+        if (isset($item[$field]) && is_string($item[$field])) {
+          $item[$field] = str_replace(' (inactive)', '', $item[$field]);
+        }
+      }
+
+      // First try exact matches on the fields.
+      foreach ($fields_to_check as $field) {
+        if (!empty($item[$field])) {
+          $term = $this->loadSourceTermByName($item[$field]);
+          if ($term) {
+            break;
+          }
+
+          $term = $this->loadSourceTermByLongName($item[$field]);
+          if ($term) {
+            break;
+          }
+
+          $term = $this->loadSourceTermByShortName($item[$field]);
+          if ($term) {
+            break;
+          }
+
+          $term = $this->loadSourceTermByAlias($item[$field]);
+          if ($term) {
+            break;
+          }
+        }
+      }
+    }
+
+    if ($term) {
+      $import_record['tid'] = $term->id();
+      $import_record['status'] = 'success';
+      $import_record = $this->saveImportRecords($source, $id, $import_record);
+      return;
+    }
+
+    $import_record['status'] = 'skipped';
+    $import_record = $this->saveImportRecords($source, $id, $import_record);
+  }
+
+  /**
+   * Merges an organization term based on a HPC item.
+   */
+  protected function mergeHpcOrganizationTerm($item) {
+    $source = 'hpc';
+    $id = $item['org id'];
+    $term = NULL;
+
+    $import_record = $this->getExistingImportRecord($source, $id);
+    if (empty($import_record)) {
+      $import_record = $this->constructReliefwebSyncOrgsRecord($source, $id, $item);
+    }
+
+    // List of possible fields to check for existing terms.
+    $fields_to_check = [
+      'org abbreviation',
+      'org name',
+    ];
+
+    // First try exact matches on the fields.
+    foreach ($fields_to_check as $field) {
+      if (!empty($item[$field])) {
+        $term = $this->loadSourceTermByName($item[$field]);
+        if ($term) {
+          break;
+        }
+
+        $term = $this->loadSourceTermByLongName($item[$field]);
+        if ($term) {
+          break;
+        }
+
+        $term = $this->loadSourceTermByShortName($item[$field]);
+        if ($term) {
+          break;
+        }
+
+        $term = $this->loadSourceTermByAlias($item[$field]);
+        if ($term) {
+          break;
+        }
+      }
+    }
+
+    if ($term) {
+      $import_record['tid'] = $term->id();
+      $import_record['status'] = 'success';
+      $import_record = $this->saveImportRecords($source, $id, $import_record);
+      return;
+    }
+
+    $import_record['status'] = 'skipped';
+    $import_record = $this->saveImportRecords($source, $id, $import_record);
+  }
+
+  /**
+   * Load a source taxonmy term using the name.
+   */
+  protected function loadSourceTermByName(string $name): ?Term {
+    $terms = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'name' => $name,
+        'vid' => 'source',
+      ]);
+
+    if (empty($terms)) {
+      return NULL;
+    }
+
+    return reset($terms);
+  }
+
+  /**
+   * Load a source taxonomy term using the long name.
+   */
+  protected function loadSourceTermByLongName(string $long_name): ?Term {
+    $terms = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'field_longname' => $long_name,
+        'vid' => 'source',
+      ]);
+
+    if (empty($terms)) {
+      return NULL;
+    }
+
+    return reset($terms);
+  }
+
+  /**
+   * Load a source taxonomy term using the short name.
+   */
+  protected function loadSourceTermByShortName(string $short_name): ?Term {
+    $terms = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'field_shortname' => $short_name,
+        'vid' => 'source',
+      ]);
+
+    if (empty($terms)) {
+      return NULL;
+    }
+
+    return reset($terms);
+  }
+
+  /**
+   * Load a source taxonomy term using a alias.
+   */
+  protected function loadSourceTermByAlias(string $alias): ?Term {
+    $terms = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'field_aliases' => $alias,
+        'vid' => 'source',
+      ]);
+
+    if (empty($terms)) {
+      return NULL;
+    }
+
+    return reset($terms);
+  }
+
+  /**
+   * Load a source taxonomy term using the fts_id.
+   */
+  protected function loadSourceTermByFtsId(string $fts_id): ?Term {
+    $terms = $this->entityTypeManager
+      ->getStorage('taxonomy_term')
+      ->loadByProperties([
+        'field_fts_id' => $fts_id,
+        'vid' => 'source',
+      ]);
+
+    if (empty($terms)) {
+      return NULL;
+    }
+
+    return reset($terms);
+  }
+
+  /**
+   * Construct a reliefweb_sync_orgs_records.
+   */
+  protected function constructReliefwebSyncOrgsRecord(string $source, string $id, array $item): array {
+    return [
+      'source' => $source,
+      'id' => $id,
+      'status' => $item['status'] ?? 'queued',
+      'created' => time(),
+      'changed' => time(),
+      'message' => '',
+      'csv_item' => $item,
+    ];
+  }
+
+  /**
+   * Retrieve existing records.
+   */
+  protected function getExistingImportRecord(string $source, string $id): array {
+    $record = $this->database->select('reliefweb_sync_orgs_records', 'r')
+      ->fields('r')
+      ->condition('source', $source)
+      ->condition('id', $id)
+      ->execute()
+      ?->fetch(\PDO::FETCH_ASSOC);
+
+    if (isset($record['csv_item'])) {
+      $record['csv_item'] = json_decode($record['csv_item'], TRUE);
+    }
+
+    return is_array($record) ? $record : [];
+  }
+
+  /**
+   * Save import records.
+   */
+  protected function saveImportRecords(string $source, string $id, array $record): array {
+    $existing_record = $this->getExistingImportRecord($source, $id);
+
+    // Set timestamp for changed field.
+    $record['changed'] = time();
+
+    // Serialize json data.
+    if (isset($record['csv_item'])) {
+      $record['csv_item'] = json_encode($record['csv_item']);
+    }
+
+    // Create comparison copies without the 'changed' timestamp.
+    $compare_existing = $existing_record;
+    $compare_new = $record;
+    unset($compare_existing['changed']);
+    unset($compare_new['changed']);
+
+    // Only update if the record has actually changed.
+    if (!empty($existing_record)) {
+      if ($compare_existing != $compare_new) {
+        $this->database->update('reliefweb_sync_orgs_records')
+          ->fields($record)
+          ->condition('source', $source)
+          ->condition('id', $id)
+          ->execute();
+      }
+    }
+    else {
+      // Set timestamp for created field if not provided.
+      if (!isset($record['created'])) {
+        $record['created'] = time();
+      }
+
+      // Insert new record.
+      $this->database->insert('reliefweb_sync_orgs_records')
+        ->fields($record)
+        ->execute();
+    }
+
+    return $record;
+  }
+
+}
