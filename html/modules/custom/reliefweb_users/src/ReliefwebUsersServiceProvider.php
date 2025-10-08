531d8013bf93d60fed09c1701f89adc97bd782d2
Peter Droogmans (attiks) <peter@attiks.com>
2025-10-08 12:37:18 +0200

    feat: Set destination for unmasquerade

---
+++ html/modules/custom/reliefweb_users/src/ReliefwebUsersServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\reliefweb_users;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Modifies the language manager service.
+ */
+class ReliefwebUsersServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    if ($container->hasDefinition('masquerade.callbacks')) {
+      $definition = $container->getDefinition('masquerade.callbacks');
+      $definition->setClass('Drupal\reliefweb_users\ReliefwebMasqueradeCallbacks')
+        ->addArgument(new Reference('redirect.destination'));
+    }
+  }
+
+}
