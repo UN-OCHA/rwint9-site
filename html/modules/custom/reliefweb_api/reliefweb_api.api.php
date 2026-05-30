d93f688aa9cf63e2a13bd47be35db9b757df4590
orakili <github@orakili.net>
2026-05-30 23:55:34 +0000

    feat: add flag to skip API indexing on entity save
    Refs: RW-1475

--- html/modules/custom/reliefweb_api/reliefweb_api.api.php
+++ html/modules/custom/reliefweb_api/reliefweb_api.api.php
@@ -6,6 +6,15 @@
  */
 
 /**
+ * Ephemeral entity properties used when saving content for the API index.
+ *
+ * Set on the entity before save; consumed in hook_entity_after_save():
+ * - $entity->needs_reindex (bool): queue batch re-indexing for the entity.
+ * - $entity->skip_reliefweb_api_indexing (bool): skip indexing for this save
+ *   only (index removal on entity delete is unaffected).
+ */
+
+/**
  * Perform operations after an entity has been indexed.
  *
  * Note: the database may not available anymore at this point.
